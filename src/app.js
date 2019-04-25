eb031f8bad39d985299ab736e414bd0240bc6bec
NEI0138 <daryoush_delgari@goodyear.com>
2019-04-25 14:19:43 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,111 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Defined paths for Express config
+const publicDirectory = path.join(__dirname, '../public')
+const viewsPath       = path.join(__dirname, '../templates/views')
+const partialsPath    = path.join(__dirname, '../templates/partials') 
+
+// Setup handlebars engines and view location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+// setup static directory to server
+app.use(express.static(publicDirectory ))
+
+app.get('', (req, res) => { 
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Andrew Mead'
+    })
+
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Andrew Mead - Help page'
+    })
+
+})
+
+app.get('/weather', (req, res) => {
+    
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+    
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        
+        if (error) {
+            return res.send({ error })  
+        }  
+    
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location: location,
+                address: req.query.address
+            })
+        })
+    })
+
+})
+
+app.get('/products', (req, res) => {
+    
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Help article not found',
+        name: 'Dary'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Page not found',
+        name: 'Dary'
+    })
+})
+
+const port = 3000
+app.listen(port, () => {
+    console.log('Server is up on port', port)
+})
+
+
---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('request')
+
+const forecast = (lat, long, callback) => {
+    const url = 'https://api.darksky.net/forecast/01f8edfa8fd23b60143a0f43531d20ed/' +  encodeURI(lat) + ',' + encodeURI(long)    
+
+    request({ url, json: true }, (error, { body }) => {
+    if (error) {
+        callback('Unable to connect to weather service !', undefined)
+    } else if (body.error) {
+        callback('Unable to find location !', undefined)
+    } 
+    else {
+             callback(undefined, { 
+                summary:  body.daily.data[0].summary,
+                temperature: body.currently.temperature,
+                precipProbability: body.currently.precipProbability
+            })
+        }
+    }) 
+
+}
+
+module.exports = forecast
